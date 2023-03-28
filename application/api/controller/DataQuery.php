3d1dcfcbb514ae2bd1a3d78fa3c0a10376f17b5a
cmqcmq <chenmq@jifenbang.net>
2023-03-28 18:45:34 +0800

    地区查询接口

--- application/api/controller/DataQuery.php
+++ application/api/controller/DataQuery.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by VSCode.
  * User: cattong
@@ -14,23 +15,63 @@ use think\Log;
 
 class DataQuery extends Base
 {
-    /**
-     * @param string $format, 返回的格式，方便前台显示，支持:raw(原生不转化), weui,mui
-     * @return \think\response\Json
-     */
     public function areas()
     {
-        $pid = input('pid/s', '0');
-        // $drilldown = input('drilldown/s', '');
+        $pid = input('pid/d', '0');
+        $drilldown = input('drilldown/d', 0);
 
         $RegionModel = new RegionModel();
-        $where[] = ['pid', '=', $pid];
-        $list  = $RegionModel->where($where)->select();
-      
+        if ($drilldown > 1) {
+            $list = cache('area_pid_' . $pid . 'drilldown_' . $drilldown);
+            if ($list) {
+                return ajax_return(ResultCode::ACTION_SUCCESS, '操作成功!', $list);
+            }
+
+            if ($drilldown == 2) {
+                $list = $RegionModel->where('pid', '=', $pid)->select();
+                foreach ($list as $key => $value) {
+                    $list[$key]['children'] = $RegionModel->where('pid', '=', $value['id'])->select();
+                }
+            } elseif ($drilldown == 3) {
+                $list = $RegionModel->select();
+                $list = $this->getTree($list, $pid, 'id', 'pid', $drilldown);
+            }
+            cache('area_pid_' . $pid . 'drilldown_' . $drilldown, $list);
+        } else {
+            $where[] = ['pid', '=', $pid];
+            $list  = $RegionModel->where($where)->select();
+        }
+
         $list = parse_fields($list, 1);
 
         return ajax_return(ResultCode::ACTION_SUCCESS, '操作成功!', $list);
-
     }
 
+    //获取树状结构
+    protected function getTree($data, $pid = 0, $fieldPK = 'id', $fieldPid = 'pid', $depth = 1, $currentDepth = 1)
+    {
+        if (empty($data)) {
+            return array();
+        }
+
+        $arr = array();
+        foreach ($data as $v) {
+            if ($v[$fieldPid] == $pid) {
+                $arr[$v[$fieldPK]] = $v;
+                $arr[$v[$fieldPK]]['level'] = $currentDepth;
+                $children = $this->getTree($data, $v[$fieldPK], $fieldPK, $fieldPid, $depth, $currentDepth + 1);
+
+                if (empty($children)) {
+                    continue;
+                }
+                //判断深度
+                if ($depth == $currentDepth) {
+                    continue;
+                }
+                $arr[$v[$fieldPK]]['children'] = $children;
+            }
+        }
+
+        return array_merge($arr);
+    }
 }
